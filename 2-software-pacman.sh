c10ca9576b4a39befb9287ba8fac7db5ac6eb290
Abdon Morales <39339393+Server2356@users.noreply.github.com>
2020-12-11 10:41:29 -0600

    Update 2-software-pacman.sh

--- 2-software-pacman.sh
+++ 2-software-pacman.sh
@@ -4,7 +4,7 @@
 #     /_\  _ _ __| |_ |  \/  |__ _| |_(_)__
 #    / _ \| '_/ _| ' \| |\/| / _` |  _| / _|
 #   /_/ \_\_| \__|_||_|_|  |_\__,_|\__|_\__|
-#  Arch Linux Post Install Setup and Config
+#  Sun/OS Installer and Post-Installer for archios and Sun/OS
 #-------------------------------------------------------------------------
 
 echo -e "\nINSTALLING SOFTWARE\n"
