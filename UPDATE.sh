652bcb79b8bc44b1bccc420295089c7e2aab6572
Abdon Morales <39339393+Server2356@users.noreply.github.com>
2020-12-12 10:06:24 -0600

    Update UPDATE.sh

--- UPDATE.sh
+++ UPDATE.sh
@@ -1,18 +1,3 @@
-#!/bin/bash
-
-var1="Sun/OS"
-var2="Sun/OS"
-var3="arch
-var4="2021.01.01"
-var5="Mount Bonnell
-var6="38;2;23;147;209"
-var7="archlinux"
-cat > /etc/os-release << EOF1
-NAME="$var1"
-PRETTY_NAME="$var2"
-ID="$var3"
-VERSION="$var4"
-VERSION_CODENAME="$var5"
-ANSI_COLOR="$var6"
-LOGO="$var7"
-EOF1
+sudo nano /etc/os-release
+sed -i '4s/2020.12.09/2021.01.01' /etc/os-release
+sed -i '5s/Mount Bonnell/Waco' /etc/os-release
